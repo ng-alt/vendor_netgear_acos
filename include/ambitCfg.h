096aed064fd50bc0b7df32e37cfca65f9d843c1e
ccc <cadappl@gmail.com>
2015-09-30 11:31:44 +0800

    Import R8500-V1.0.0.42_1.0.23
    Change-Id: I4cdd1c0e54d2f8b10c3b317ca57750a18e8d133e

--- include/ambitCfg.h
+++ include/ambitCfg.h
@@ -47,9 +47,9 @@
 
 /*formal version control*/
 #define AMBIT_HARDWARE_VERSION     "U12H334T00"
-#define AMBIT_SOFTWARE_VERSION     "V1.0.0.28.8"
-#define AMBIT_UI_VERSION           "1.0.15"
-#define STRING_TBL_VERSION         "1.0.0.28_2.1.46.1"
+#define AMBIT_SOFTWARE_VERSION     "V1.0.0.42"
+#define AMBIT_UI_VERSION           "1.0.23"
+#define STRING_TBL_VERSION         "1.0.0.42_2.1.46.1"
 
 #define AMBIT_PRODUCT_NAME          "R7800"
 #define AMBIT_PRODUCT_ALIAS         "Nighthawk X8"
