5fb2e2f080e3ffe17b3137107efe4c939a23066c
ccc <cadappl@gmail.com>
2017-12-11 21:51:08 +0800

    Import R7000-V1.0.9.18_1.2.27
    Change-Id: I845954e9d2680efbcb18a89e831bc6b4b5b0dfaf

--- include/ambitCfg.h
+++ include/ambitCfg.h
@@ -50,9 +50,9 @@
 
 /*formal version control*/
 #define AMBIT_HARDWARE_VERSION     "U12H270T00"
-#define AMBIT_SOFTWARE_VERSION     "V1.0.9.14"
-#define AMBIT_UI_VERSION           "1.2.25"
-#define STRING_TBL_VERSION         "1.0.9.14_2.1.38.1"
+#define AMBIT_SOFTWARE_VERSION     "V1.0.9.18"
+#define AMBIT_UI_VERSION           "1.2.27"
+#define STRING_TBL_VERSION         "1.0.9.18_2.1.38.1"
 
 #define AMBIT_PRODUCT_NAME          "R7000"
 #define AMBIT_PRODUCT_DESCRIPTION   "802.11ac Dual Band Gigabit Wireless Router R7000"
