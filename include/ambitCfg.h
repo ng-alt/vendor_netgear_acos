627b687feff25dc8382453558ced1d9a0b9135e3
ccc <cadappl@gmail.com>
2017-12-15 15:41:51 +0800

    Import R6400-V1.0.1.34_1.0.24
    Change-Id: Idd12f7e98d88fa1c74bc7efd41c40b8908abd2b4

--- include/ambitCfg.h
+++ include/ambitCfg.h
@@ -47,9 +47,9 @@
 
 /*formal version control*/
 #define AMBIT_HARDWARE_VERSION     "U12H332T00"
-#define AMBIT_SOFTWARE_VERSION     "V1.0.1.32"
-#define AMBIT_UI_VERSION           "1.0.23"
-#define STRING_TBL_VERSION         "1.0.1.32_2.1.38.1"
+#define AMBIT_SOFTWARE_VERSION     "V1.0.1.34"
+#define AMBIT_UI_VERSION           "1.0.24"
+#define STRING_TBL_VERSION         "1.0.1.34_2.1.38.1"
 
 #define AMBIT_PRODUCT_NAME          "R6400"
 #define AMBIT_PRODUCT_DESCRIPTION   "802.11ac Dual Band Gigabit Wireless Router R6400"
